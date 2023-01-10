d4497e5ed6f9de6134afba0fc02ac98211a4599c
Pakywka <sysoev.edik@gmail.com>
2023-01-10 22:15:39 +0300

    .

--- frontend/server.js
+++ frontend/server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(compression());
 
-app.use(express.static(`${__dirname}/build`));
+app.use(express.static('build'));
 
 app.listen(process.env.PORT || 3000, () =>
     console.log(`Server started on PORT = ${process.env.PORT || 3000}`),
