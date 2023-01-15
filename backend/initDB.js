8acd0d89f6ad7ddfda2b8784d7da98aa21ee50d4
Pakywka <sysoev.edik@gmail.com>
2023-01-15 19:06:24 +0300

    .

--- backend/initDB.js
+++ backend/initDB.js
@@ -4,8 +4,6 @@ module.exports = () => {
     mongoose
         .connect(process.env.MONGODB_URI, {
             dbName: process.env.DB_NAME,
-            user: process.env.DB_USER,
-            pass: process.env.DB_PASSWORD,
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
