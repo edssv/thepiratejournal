8f995027f145ab63d2eb979ae19e4aeac4b95c14
Pakywka <sysoev.edik@gmail.com>
2023-04-06 00:31:53 +0300

    layout, file controller

--- server/src/modules/file/file.controller.ts
+++ server/src/modules/file/file.controller.ts
@@ -1,16 +1,14 @@
 import { Controller, Post, UseInterceptors, UploadedFile, UseGuards } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { JwtAuthGuard } from 'src/modules/auth/guards/jwt-auth.guard';
 import { FileService } from './file.service';
 
 @Controller('files')
 export class FileController {
-    constructor(private readonly fileService: FileService) {}
+  constructor(private readonly fileService: FileService) {}
 
-    @UseGuards(JwtAuthGuard)
-    @Post('upload')
-    @UseInterceptors(FileInterceptor('image'))
-    uploadFile(@UploadedFile() file: Express.Multer.File) {
-        return this.fileService.uploadFile(file);
-    }
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('image'))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return this.fileService.uploadFile(file);
+  }
 }
