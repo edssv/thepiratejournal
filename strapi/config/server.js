485578b33796720e4ff4c7ebe43ac738a7427e03
Eduard Sysoev <syspev.edik@gmail.com>
2024-03-19 16:46:09 +0300

    chore: 🔧 update server.js file

--- strapi/config/server.js
+++ strapi/config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
-  url: env("STRAPI_API_URL"),
+  port: env.int("PORT", 1338),
+  url: env("URL", "http://localhost:1338"),
   app: {
     keys: env.array("APP_KEYS"),
   },
